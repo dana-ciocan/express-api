fd0258b923c7bc799661a6d92b87c410fa20487d
Dana Ciocan <danaciocan@economist.com>
2022-10-31 20:52:07 +0000

    Create logger middleware

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ const express = require('express');
 const dotenv = require('dotenv');
 
 const bootcamps = require('./routes/bootcamps');
+const logger = require('./middleware/logger');
 
 dotenv.config({
   path: './config/config.env',
 });
 
 const app = express();
+
+app.use(logger);
 
 app.use('/api/v1/bootcamps', bootcamps);
 
